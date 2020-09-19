2945281680fd3dc5fff411a5883a060cf96f3408
Egor <steammycss@gmail.com>
2020-09-19 22:11:02 +0400

    little fixes

--- Server/preproc_rts.py
+++ Server/preproc_rts.py
@@ -3,7 +3,9 @@ import ruamel.yaml
 
 import sys
 import os
+from pprint import PrettyPrinter
 
+pp = PrettyPrinter(4)
 yaml = ruamel.yaml.YAML()
 yaml.register_class(RT)
 
@@ -11,11 +13,12 @@ def main(rts_path):
     if os.path.exists(rts_path):
         with open(rts_path, 'rb') as fstream:
             rts = yaml.load(fstream)
-        for comment, body in rts:
+        for comment, body in rts.items():
             if 'section' in body:
-                body['SOPairs'] = [(body['section'], body['oucome'])]
-                body.remove('section')
-                body.remove('outcome')
+                body['SOPairs'] = [(body['section'], body['outcome'])]
+                del body['section']
+                del body['outcome']
+            pp.pprint({comment: body})
         with open(rts_path + '.ed', 'wb') as fstream:
             yaml.dump(rts, fstream)
     else:
